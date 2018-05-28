f0066f584d2afa66bf9091006d5a8bb492807c55
deva jayantha <devajayantha870@gmail.com>
2018-05-28 22:28:16 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
 		
-		<title>Compass Starter by Ariona, Rian</title>
-
+		<title>Internet Of Things Weather</title>
+		<link rel="stylesheet" href="{{asset('css/app.css')}}">
+		<link rel="stylesheet" href="{{asset('css/main.css')}}">
 		<!-- Loading third party fonts -->
 		<link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/light/all.min.css" />
 		<script type="text/javascript" src="http://www.shieldui.com/shared/components/latest/js/shieldui-all.min.js"></script>
@@ -15,55 +16,46 @@
 		<link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
 		<link href="{{ asset('theme/fonts/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
 
+
 		<!-- Loading main css file -->
 		<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 		
-		<!--[if lt IE 9]>
+		{{--  <!--[if lt IE 9]>
 		<script src="js/ie-support/html5.js"></script>
 		<script src="js/ie-support/respond.js"></script>
-		<![endif]-->
+		<![endif]-->  --}}
 
 	</head>
 
 	<body>
-		
-		<div class="site-content">
-			<div class="site-header">
-				<div class="container">
-					<a href="index.html" class="branding">
-						<img src="images/logo.png" alt="" class="logo">
-						<div class="logo-type">
-							<h1 class="site-title">Company name</h1>
-							<small class="site-description">tagline goes here</small>
+			<div class="site-content">
+				<div class="site-header header-nav">
+					<div class="container">
+						<a href="index.html" class="branding">
+							<img src="images/logo.png" alt="" class="logo">
+							<div class="logo-type">
+								<h1 class="site-title">Weather Station</h1>
+								<small class="site-description">Internet of Things project</small>
+							</div>
+						</a>
+						{{--  navbar  --}}
+						<div class="main-navigation">
+							<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+							<ul class="menu">
+								<li class="menu-item current-menu-item"><a href="/">Home</a></li>
+							</ul>
 						</div>
-					</a>
-
-					<!-- Default snippet for navigation -->
-					<div class="main-navigation">
-						<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
-						<ul class="menu">
-							<li class="menu-item current-menu-item"><a href="index.html">Home</a></li>
-							<li class="menu-item"><a href="news.html">News</a></li>
-							<li class="menu-item"><a href="live-cameras.html">Live cameras</a></li>
-							<li class="menu-item"><a href="photos.html">Photos</a></li>
-							<li class="menu-item"><a href="contact.html">Contact</a></li>
-						</ul> <!-- .menu -->
-					</div> <!-- .main-navigation -->
-
-					<div class="mobile-navigation"></div>
-
+					</div>
 				</div>
-			</div> <!-- .site-header -->
 
-			
 
-			<div class="forecast-table">
+			<div class="forecast-table forest-margin">
 				<div class="container">
 					<div class="forecast-container">
 						<div class="today forecast">
 						
 							<div class="forecast-header">
-								<div class="day">Tuesday</div>
+								<div class="day">Rainfalls</div>
 								<div class="date">24 Apr 2018 </div>
 							</div> <!-- .forecast-header -->
 							<div class="forecast-content">
@@ -76,17 +68,17 @@
 									</div>	
 								</div>
 								<span><img src="images/icon-umberella.png" alt="">%</span>
-								<!-- @if(count($widths)) -->
-								@foreach($widths as $wd)
-								<span><img src="images/icon-wind.png" alt="">{{ $wd -> $width }}km/h</span>
-								@endforeach
-								<!-- @endif -->
+								{{-- <!-- @if(count($widths)) --> --}}
+								{{-- @foreach($widths as $wd) --}}
+								<span><img src="images/icon-wind.png" alt="">22km/h</span>
+								{{-- @endforeach
+								<!-- @endif --> --}}
 								<span><img src="images/icon-compass.png" alt="">East</span>
 							</div>
 						</div>
 						<div class="today forecast">
 							<div class="forecast-header">
-								<div class="day">Tuesday</div>
+								<div class="day">Humidities</div>
 								<div class="date">24 Apr 2018</div>
 							</div> <!-- .forecast-header -->
 							<div class="forecast-content">
@@ -106,95 +98,60 @@
 					</div>
 				</div>
 			</div>
-	
-				<div class="fullwidth-block" data-bg-color="#262936">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-4">
-								<div class="news">
-									<div class="date">06.10</div>
-									<h3><a href="#">Doloremque laudantium totam sequi </a></h3>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo saepe assumenda dolorem modi, expedita voluptatum ducimus necessitatibus. Asperiores quod reprehenderit necessitatibus harum, mollitia, odit et consequatur maxime nisi amet doloremque.</p>
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="news">
-									<div class="date">06.10</div>
-									<h3><a href="#">Doloremque laudantium totam sequi </a></h3>
-									<p>Nobis architecto consequatur ab, ea, eum autem aperiam accusantium placeat vitae facere explicabo temporibus minus distinctio cum optio quis, dignissimos eius aspernatur fuga. Praesentium totam, corrupti beatae amet expedita veritatis.</p>
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="news">
-									<div class="date">06.10</div>
-									<h3><a href="#">Doloremque laudantium totam sequi </a></h3>
-									<p>Enim impedit officiis placeat qui recusandae doloremque possimus, iusto blanditiis, quam optio delectus maiores. Possimus rerum, velit cum natus eos. Cumque pariatur beatae asperiores, esse libero quas ad dolorem. Voluptates.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
 
-				<div class="fullwidth-block">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-4">
-								<h2 class="section-title">Application features</h2>
-								<ul class="arrow-feature">
-									<li>
-										<h3>Natus error sit voluptatem accusantium</h3>
-										<p>Doloremque laudantium totam rem aperiam Inventore veritatis et quasi architecto beatae vitae.</p>
-									</li>
-									<li>
-										<h3>Natus error sit voluptatem accusantium</h3>
-										<p>Doloremque laudantium totam rem aperiam Inventore veritatis et quasi architecto beatae vitae.</p>
-									</li>
-									<li>
-										<h3>Natus error sit voluptatem accusantium</h3>
-										<p>Doloremque laudantium totam rem aperiam Inventore veritatis et quasi architecto beatae vitae.</p>
-									</li>
-								</ul>
-							</div>
-							<div class="col-md-4">
-								<h2 class="section-title">Weather analyssis</h2>
-								<ul class="arrow-list">
-									<li><a href="#">Accusantium doloremque laudantium rem aperiam</a></li>
-									<li><a href="#">Eaque ipsa quae ab illo inventore veritatis quasi</a></li>
-									<li><a href="#">Architecto beatae vitae dicta sunt explicabo</a></li>
-									<li><a href="#">Nemo enim ipsam voluptatem quia voluptas</a></li>
-									<li><a href="#">Aspernatur aut odit aut fugit, sed quia consequuntur</a></li>
-									<li><a href="#">Magni dolores eos qui ratione voluptatem sequi</a></li>
-									<li><a href="#">Neque porro quisquam est qui dolorem ipsum quia</a></li>
-								</ul>
-							</div>
-							<div class="col-md-4">
-								<h2 class="section-title">Awesome Photos</h2>
-								<div class="photo-grid">
-									<a href="#"><img src="images/thumb-1.jpg" alt="#"></a>
-									<a href="#"><img src="images/thumb-2.jpg" alt="#"></a>
-									<a href="#"><img src="images/thumb-3.jpg" alt="#"></a>
-									<a href="#"><img src="images/thumb-4.jpg" alt="#"></a>
-									<a href="#"><img src="images/thumb-5.jpg" alt="#"></a>
-									<a href="#"><img src="images/thumb-6.jpg" alt="#"></a>
-									<a href="#"><img src="images/thumb-7.jpg" alt="#"></a>
-									<a href="#"><img src="images/thumb-8.jpg" alt="#"></a>
-									<a href="#"><img src="images/thumb-9.jpg" alt="#"></a>
+			<div class="forecast-table forest-margin">
+				<div class="container">
+					<div class="forecast-container">
+						<div class="today forecast">
+						
+							<div class="forecast-header">
+								<div class="day">Widths</div>
+								<div class="date">24 Apr 2018 </div>
+							</div> <!-- .forecast-header -->
+							<div class="forecast-content">
+								<div class="location">Bukit</div>
+								
+								<div class="degree">
+									<div class="num"> &nbspM</div>
+									<div class="forecast-icon">
+										<img src="images/icons/icon-1.svg" alt="" width=90>
+									</div>	
 								</div>
+								<span><img src="images/icon-umberella.png" alt="">%</span>
+								{{-- <!-- @if(count($widths)) --> --}}
+								{{-- @foreach($widths as $wd) --}}
+								<span><img src="images/icon-wind.png" alt="">22km/h</span>
+								{{-- @endforeach
+								<!-- @endif --> --}}
+								<span><img src="images/icon-compass.png" alt="">East</span>
+							</div>
+						</div>
+						<div class="today forecast">
+							<div class="forecast-header">
+								<div class="day">Temperatures</div>
+								<div class="date">24 Apr 2018</div>
+							</div> <!-- .forecast-header -->
+							<div class="forecast-content">
+								<div class="location">Bukit</div>
+								<div class="degree">
+									<div class="num">
+										<sup>o</sup>C</div>
+									<div class="forecast-icon">
+										<img src="images/icons/icon-1.svg" alt="" width=90>
+									</div>	
+								</div>
+								<span><img src="images/icon-umberella.png" alt="">%</span>
+								<span><img src="images/icon-wind.png" alt="">km/h</span>
+								<span><img src="images/icon-compass.png" alt="">East</span>
 							</div>
 						</div>
 					</div>
 				</div>
-			</main> <!-- .main-content -->
+			</div>
 
 			<footer class="site-footer">
 				<div class="container">
 					<div class="row">
-						<div class="col-md-8">
-							<form action="#" class="subscribe-form">
-								<input type="text" placeholder="Enter your email to subscribe...">
-								<input type="submit" value="Subscribe">
-							</form>
-						</div>
 						<div class="col-md-3 col-md-offset-1">
 							<div class="social-links">
 								<a href="#"><i class="fa fa-facebook"></i></a>
@@ -204,15 +161,51 @@
 							</div>
 						</div>
 					</div>
-
-					<p class="colophon">Copyright 2014 Company name. Designed by Themezy. All rights reserved</p>
+					<p class="colophon">Copyright 2018 Weather Stations. All rights reserved</p>
 				</div>
-			</footer> <!-- .site-footer -->
+			</footer>
 		</div>
-		
-		<script src="{{ asset('theme/js/jquery-1.11.1.min.js') }}"></script>
+		<script src="{{ asset('theme/js/app.js') }}"></script>	
+		{{-- <script src="{{ asset('theme/js/jquery-1.11.1.min.js') }}"></script> --}}
 		<script src="{{ asset('theme/js/plugins.js') }}"></script>
-		<script src="{{ asset('theme/js/app.js') }}"></script>
+		<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-app.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-auth.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-database.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-firestore.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-messaging.js"></script>
+		{{--  firebase js nya   --}}
+		<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-storage.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase.js"></script>
+		<script>
+		  // Initialize Firebase
+		  var config = {
+			apiKey: "AIzaSyD31ZLIBbyPB4UFYCkzebSlcL7AmniZgPg",
+			authDomain: "weatherstation-97401.firebaseapp.com",
+			databaseURL: "https://weatherstation-97401.firebaseio.com",
+			projectId: "weatherstation-97401",
+			storageBucket: "weatherstation-97401.appspot.com",
+			messagingSenderId: "494211813974"
+		  };
+		  firebase.initializeApp(config);
+
+		  //NoSQL
+
+		  var db = firebase.database();
+		  var widthsRef = db.ref('widths');
+
+
+		  widthsRef.on('value',showData,showEror);
+
+
+		  function showData(items){
+			  console.log(items.val())
+		  }
+			function showEror(err){
+				console.log(err)
+			}
+		  
+
+		</script>
 		
 	</body>
 
